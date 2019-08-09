efb1d124707bc40026f22d5a583928519eeda410
Turar Abu <erfalon@gmail.com>
2019-08-09 11:31:04 +1000

    seventh 09.08.19

--- src/components/Filter/Range.vue
+++ src/components/Filter/Range.vue
@@ -46,8 +46,8 @@ function init () {
     var last
     var range = this.$el
 
-    document.addEventListener('touchstart', () => { this.active = false })
-    document.addEventListener('touchend', event => { last = event.touches[0] })
+    document.addEventListener('touchend', () => { this.active = false })
+    document.addEventListener('touchstart', event => { last = event.touches[0] })
     document.addEventListener('touchmove', event => {
         if ( this.active === false )
             return
